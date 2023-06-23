819dae862fbf417d2aef2009c05020ef8cdefff4
davipaluch <davipaluch@icloud.com>
2023-06-23 16:12:31 -0300

    New chapter for the repository

---
+++ use-a-cabeca-capitulo-08/use-a-cabeca-cap-08/Duck.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace use_a_cabeca_cap_08
+{
+    internal class Duck 
+    {
+        public int Size;
+        public KindOfDuck Kind;
+
+        public Duck(int size, KindOfDuck kind)
+        {
+            Size = size;
+            Kind = kind;
+        }
+
+        public string ToString()
+        {
+            return (this.Kind.ToString() + " " + this.Size.ToString());
+        }
+
+        
+    }
+
+    public class DuckComparer_bySize : IComparer<Duck>
+    {
+        int IComparer<Duck>.Compare(Duck x, Duck y)
+        {
+            if (x.Size < y.Size)
+                return -1;
+            if (x.Size > y.Size)
+                return 1;
+            return 0;
+        }
+    }
+    public class DuckComparer_byKind : IComparer<Duck>
+    {
+        int IComparer<Duck>.Compare(Duck x, Duck y)
+        {
+            if (x.Kind < y.Kind)
+                return -1;
+            if (x.Kind > y.Kind)
+                return 1;
+            return 0;
+        }
+    }
+
+    public class DuckComparer : IComparer<Duck>
+    {
+        public enum SortCriteria
+        {
+            SizeThenKind,
+            KindThenSize
+
+        }
+        public SortCriteria SortBy = SortCriteria.SizeThenKind;
+
+
+        int IComparer<Duck>.Compare(Duck x, Duck y)
+        {
+            if (SortBy == SortCriteria.SizeThenKind)
+            {
+                if (x.Size < y.Size)
+                    return -1;
+                else if (x.Size > y.Size)
+                    return 1;
+                else
+                {
+                    if (x.Kind > y.Kind)
+                        return 1;
+                    else if(x.Kind < y.Kind)
+                        return -1;
+                    else
+                        return 0;
+                }
+            }
+            else
+            {
+                if (x.Kind > y.Kind)
+                    return 1;
+                else if(x.Kind < y.Kind)
+                    return -1;
+                else 
+                    return 0;
+            }
+
+        }
+
+    }
+
+
+
+
+
+
+    public enum KindOfDuck
+    {
+        Mallard,
+        Muscovy,
+        Decoy
+    }
+}
---
+++ use-a-cabeca-capitulo-08/use-a-cabeca-cap-08/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace use_a_cabeca_cap_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Duck> ducks = new List<Duck>()
+            {
+                new Duck( 17,KindOfDuck.Mallard),
+                new Duck( 18,KindOfDuck.Muscovy),
+                new Duck( 14,KindOfDuck.Decoy),
+                new Duck( 11,KindOfDuck.Muscovy),
+                new Duck( 14,KindOfDuck.Mallard),
+                new Duck( 13,KindOfDuck.Decoy)
+            };
+
+
+
+            Console.WriteLine("Lista Sem Ordenação: ");
+            foreach (Duck duck in ducks)
+                Console.WriteLine(duck.ToString());
+
+
+
+            DuckComparer_bySize duckComparer_BySize = new DuckComparer_bySize();
+
+            ducks.Sort(duckComparer_BySize);
+
+
+
+            Console.WriteLine("Lista Ordenada Por Peso: ");
+            foreach (Duck duck in ducks)
+                Console.WriteLine(duck.ToString());
+
+
+
+
+
+            DuckComparer_byKind duckComparer_ByKind = new DuckComparer_byKind();
+
+            ducks.Sort(duckComparer_ByKind);
+
+
+            Console.WriteLine("Lista Ordenada por Tipo de Pato: ");
+            foreach (Duck duck in ducks)
+                Console.WriteLine(duck.ToString());
+
+
+
+            DuckComparer duckComparer = new DuckComparer();
+            ducks.Sort(duckComparer);
+
+            Console.WriteLine("Lista Ordenada por Comparação Complexa: ");
+            foreach (Duck duck in ducks)
+                Console.WriteLine(duck.ToString());
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
